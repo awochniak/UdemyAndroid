691ab758b1d61b02a8db50e4812f036ab205ca60
awochniak <arkadiusz.wochniak@gmail.com>
2018-12-21 14:14:16 +0100

    ukończenie zapisu i odczytu danych z pamięci

--- app/src/main/java/com/example/arkadiuszwochniak/udemyandroid/ShoppingListActivity.java
+++ app/src/main/java/com/example/arkadiuszwochniak/udemyandroid/ShoppingListActivity.java
@@ -3,7 +3,6 @@ package com.example.arkadiuszwochniak.udemyandroid;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.ArraySet;
@@ -30,7 +29,7 @@ public class ShoppingListActivity extends AppCompatActivity {
     private Set<String> spinnerItems;
 
     public static final String LIST_ITEMS_KEY = "LIST_ITEMS_KEY";
-    public static final String SPINNER_ITEMS_KEY = "LIST_SPINNER_KEY";
+    public static final String SPINNER_ITEMS_KEY = "SPINNER_ITEMS_KEY";
     public static final String SHOPPING_LIST_KEY = "SHOPPING_LIST_KEY";
 
     @Override
@@ -40,8 +39,8 @@ public class ShoppingListActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        listItems = new ArraySet<String>()''
-        spinnerItems = new ArraySet<String>()''
+        listItems = new ArraySet<String>();
+        spinnerItems = new ArraySet<String>();
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
 
@@ -58,15 +57,10 @@ public class ShoppingListActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         SharedPreferences.Editor editor = getSharedPreferences(SHOPPING_LIST_KEY, MODE_PRIVATE).edit(); //import static constant
-        editor.putString("", "inna super tajna wiadomość");
+        editor.putString(LIST_ITEMS_KEY, listItems);
+        editor.putString(SPINNER_ITEMS_KEY, spinnerItems);
         editor.apply();
     }
 
-    /**
-     *   SharedPreferences xd = getSharedPreferences(sp, MODE_PRIVATE);
-     *             String message = xd.getString(sp_key,null);
-     *             if (message!=null) {
-     *                 Toast.makeText(this, message, Toast.LENGTH_LONG).show();
-     *             }
-     */
+  
 }
